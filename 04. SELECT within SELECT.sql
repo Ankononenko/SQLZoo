6d1725f1bd8348c099a4eb81231ae27024cbbf78
Anton <88505337+finchren@users.noreply.github.com>
2023-01-07 21:46:06 +0300

    Update 04. SELECT within SELECT.sql

--- 04. SELECT within SELECT.sql
+++ 04. SELECT within SELECT.sql
@@ -10,9 +10,11 @@ WHERE population > (
 		FROM world
 		WHERE NAME = "Russia"
 		)
-    
+
+/*
 Richer than UK
 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+*/
     
 SELECT NAME
 FROM world
@@ -22,4 +24,39 @@ WHERE gdp / population > (
 		WHERE NAME = "United Kingdom"
 		)
 	AND continent = "Europe"
+
+/*
+Neighbours of Argentina and Australia
+3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+*/
+
+SELECT NAME
+	,continent
+FROM world
+WHERE continent IN (
+		SELECT continent
+		FROM world
+		WHERE NAME = "Argentina"
+			OR NAME = "Australia"
+		)
+ORDER BY NAME
+
+/*
+Between United Kingdom and Germany
+4. Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population.
+*/
+
+SELECT NAME
+	,population
+FROM world
+WHERE population > (
+		SELECT population
+		FROM world
+		WHERE NAME = "United Kingdom"
+		)
+	AND population < (
+		SELECT population
+		FROM world
+		WHERE NAME = "Germany"
+		)
 
