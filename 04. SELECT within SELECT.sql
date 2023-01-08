3f815bfc9b47fe63d806a0021cb40540b174c7e8
Anton <88505337+finchren@users.noreply.github.com>
2023-01-08 08:39:28 +0300

    Saving midway. Almost done with the exercise

--- 04. SELECT within SELECT.sql
+++ 04. SELECT within SELECT.sql
@@ -60,3 +60,37 @@ WHERE population > (
 		WHERE NAME = "Germany"
 		)
 
+/*
+Percentages of Germany
+5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+The format should be Name, Percentage.
+*/
+
+SELECT NAME
+	,CONCAT (
+		round(population / (
+				SELECT population
+				FROM world
+				WHERE NAME = "Germany"
+				) * 100)
+		,"%"
+		) AS percentage
+FROM world
+WHERE continent = "Europe"
+
+/*
+Bigger than every country in Europe
+6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+*/
+
+SELECT NAME
+FROM world
+WHERE gdp >= ALL (
+		SELECT gdp
+		FROM world
+		WHERE continent = "Europe"
+			AND gdp > 0
+		)
+	AND continent != "Europe"
+
